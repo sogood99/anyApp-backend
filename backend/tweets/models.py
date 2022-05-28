c9a95902e548ede694feee7f8475019f14069e58
Onii Chan <46487694+sogood99@users.noreply.github.com>
2022-05-29 03:09:26 +0800

    Add in Like Model

--- backend/tweets/models.py
+++ backend/tweets/models.py
@@ -5,8 +5,32 @@ from django.contrib.auth.models import User
 class Tweet(models.Model):
     """
         The tweet model
+        @param user User that send the tweet
+        @param text Text field of the tweet
+        @param imageUrl Url for image (nullable)
+        @param videoUrl Url for video (nullable)
+        @param replies If this tweet is a reply of another tweet, foriegn key ref that tweet
     """
-    user = models.ForeignKey(User)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     text = models.CharField(max_length=150)
-    image_url = models.CharField(max_length=100, null=True, blank=True)
-    video_url = models.CharField(max_length=100, null=True, blank=True)
+    imageUrl = models.CharField(max_length=100, null=True, blank=True)
+    videoUrl = models.CharField(max_length=100, null=True, blank=True)
+    replies = models.ForeignKey(
+        "Tweet", on_delete=models.CASCADE, null=True, blank=True)
+
+
+class Like(models.Model):
+    """
+        Like model
+        @param user User that liked
+        @param tweet Tweet that the user liked
+    """
+    user = models.ForeignKey(User,  on_delete=models.CASCADE)
+    tweet = models.ForeignKey(Tweet, on_delete=models.CASCADE)
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=['user', 'tweet'], name='unique_like_for_user_tweet'
+            )
+        ]
