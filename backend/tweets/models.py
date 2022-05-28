4976760816fdab25181ec604a85aae9efc2095b8
Onii Chan <46487694+sogood99@users.noreply.github.com>
2022-05-28 14:25:34 +0800

    Add Tweet Model

--- backend/tweets/models.py
+++ backend/tweets/models.py
@@ -2,4 +2,11 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
-# Create your models here.
+class Tweet(models.Model):
+    """
+        The tweet model
+    """
+    user = models.ForeignKey(User)
+    text = models.CharField(max_length=150)
+    image_url = models.CharField(max_length=100, null=True, blank=True)
+    video_url = models.CharField(max_length=100, null=True, blank=True)
