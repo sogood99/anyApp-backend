9443c64cdc42947484115fb6dffe38ec6b443275
Onii Chan <46487694+sogood99@users.noreply.github.com>
2022-06-01 10:39:40 +0800

    Tweet Serializer to send UserIcon

--- backend/tweets/serializers.py
+++ backend/tweets/serializers.py
@@ -2,13 +2,16 @@ from rest_framework import serializers
 from rest_framework.validators import UniqueValidator
 from django.contrib.auth.models import User
 
+from accounts.models import Profile
 from tweets.models import Tweet
 
 
 class TweetSerializer(serializers.ModelSerializer):
-    tweetId = serializers.IntegerField(source='id')
-    userId = serializers.IntegerField(source='user.id')
-    username = serializers.CharField(source='user.username')
+    tweetId = serializers.IntegerField(source='id', read_only=True)
+    userId = serializers.IntegerField(source='user.id', read_only=True)
+    username = serializers.CharField(source='user.username', read_only=True)
+    userIconUrl = serializers.SerializerMethodField(
+        'getUserIconUrl', read_only=True)
     text = serializers.CharField()
     imageUrl = serializers.CharField()
     videoUrl = serializers.CharField()
@@ -17,5 +20,12 @@ class TweetSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Tweet
-        fields = ('tweetId', 'userId', 'username',
+        fields = ('tweetId', 'userId', 'username', 'userIconUrl',
                   'text', 'imageUrl', 'videoUrl', 'repliesId', 'createDate')
+
+    def getUserIconUrl(self, obj):
+        userProfile = Profile.objects.get(pk=obj.user)
+        if userProfile is not None and userProfile.userIconUrl is not None:
+            return userProfile.userIconUrl
+        else:
+            return 'image/userIcon/default.jpg'
